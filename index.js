454cc80b0dea9e411d63e53cc630658bd498333c
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-06-27 13:25:22 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+let companyName = 'Scuber';{
+const companyName = 'Scuber'; //This can cause an error since it will be a re-declaration
+}
+
+const mostProfitableNeighborhood = 'Chelsea';{
+    let mostProfitableNeighborhood = 'Chelsea';
+}
+
+const companyCeo = 'Susan Smith';{
+let companyCeo = 'Susan Smith';
+}
